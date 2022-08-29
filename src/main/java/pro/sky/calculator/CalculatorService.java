39b7cda824f655102f3f647b069cecd674c56048
Kosarev <kosarevn97@gmail.com>
2022-08-29 21:53:14 +0700

    ДЗ

--- src/main/java/pro/sky/calculator/CalculatorService.java
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -9,15 +9,15 @@ public class CalculatorService {
         return a + b;
     }
     public int minus(int a,
-                   int b) {
+                     int b) {
         return a - b;
     }
     public int multiply(int a,
-                   int b) {
+                        int b) {
         return a * b;
     }
     public double divide(int a,
-                   int b) {
+                         int b) {
         return a / (double)b;
     }
 }
